2d977ac58fb30d78dd225880c7eb1dc8b66c3ef3
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2022-10-04 11:01:19 +0200

    add .nvmrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: `https://instantsearch-templates.netlify.app/`,
+  site: "https://instantsearch-templates.netlify.app/",
 });
